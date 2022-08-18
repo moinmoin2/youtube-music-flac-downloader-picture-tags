c2350c4d82fc87739f0af076eef81dfc26d44542
moinmoin2 <48869696+moinmoin2@users.noreply.github.com>
2022-08-18 21:06:23 +0200

    V3
    Adding Release Year to tags

--- convert.sh
+++ convert.sh
@@ -5,7 +5,7 @@ echo "Youtube Music Downloader"
 echo "Post the Playlist link or link of the song"
 read url
 short_url=$(echo "$url" | sed s/"&feature=share"//)
-youtube-dl -x --audio-format flac --add-metadata --write-thumbnail --id "$short_url"
+youtube-dl -x --audio-format flac --add-metadata --write-thumbnail --id --ignore-errors "$short_url"
 
 #getting full files names
 for i in *.flac; do
@@ -20,32 +20,37 @@ for i in *.flac; do
     if [[ -f "${name}.webp" ]]; then
         #convert them to diffrent resolution
         convert "${name}.webp" -crop 720x720+280 "$finale_picture"
-        rm -v "${name}.webp"
+        rm -v "./${name}.webp"
     fi
 
   #for jpg files
     if [[ -f "${name}.jpg" ]]; then
         #convert them to diffrent resolution
         convert "${name}.jpg" -crop 720x720+280 "$finale_picture"
-        rm -v "${name}.jpg"
+        rm -v "./${name}.jpg"
     fi
+#check for release year of the song
 
+comment=$(exiftool -q -Description "./$i")
 
-artist=$(exiftool -q -s3 -Artist "$i" | cut -d',' -f1)
-#echo $artist
+if echo "$comment" | grep 'Released on:'; then
+    release_year=$(echo $comment | sed 's/.*Released on: //' | awk '{print substr ($0, 0, 4)}')
+fi
 
-title=$(exiftool -q -s3 -Title "$i")
-#echo $title
+#get artist and title for the final filename
 
+artist=$(exiftool -q -s3 -Artist "./${i}" | cut -d',' -f1)
+title=$(exiftool -q -s3 -Title "./${i}")
 final="${artist} - ${title}"
-#echo $finale
+
+
 
 #merging files
-ffmpeg -i "$i" -i "${name}new.jpg" -s 720x720 -map 0:0 -map 1:0 -disposition:v:0 attached_pic -c:v:0 mjpeg -ab 320k -map_metadata 0 -y -id3v2_version 3 "${final}.flac" 
+ffmpeg -i "./${i}" -i "${name}new.jpg" -s 720x720 -map 0:0 -map 1:0 -disposition:v:0 attached_pic -c:v:0 mjpeg -ab 320k -map_metadata 0 -metadata date="$release_year" -y -id3v2_version 3 "${final}.flac" 
 
 #deleting not needed files
-rm -v "$i"
-rm -v "$finale_picture"
+rm -v "./${i}"
+rm -v "./${finale_picture}"
 
 
 done
